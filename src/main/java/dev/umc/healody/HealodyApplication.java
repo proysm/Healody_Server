06285b8ff0a0f967792303810512b5a384a2ef01
Hyunwoo Kang <gusdn741021@gmail.com>
2023-07-08 16:01:04 +0900

    first commit

---
+++ src/main/java/dev/umc/healody/HealodyApplication.java
@@ -0,0 +1,13 @@
+package dev.umc.healody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/umc/healody/HealodyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umc.healody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
