3079475992f06edcad03201835f0f10a9594385b
유수민 <104756460+proysm@users.noreply.github.com>
2023-08-30 16:50:16 +0900

    Update KakaoUserController.java

--- src/main/java/dev/umc/healody/user/controller/KakaoUserController.java
+++ src/main/java/dev/umc/healody/user/controller/KakaoUserController.java
@@ -14,7 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 
-@Controller
+@Controller    
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class KakaoUserController {
 
